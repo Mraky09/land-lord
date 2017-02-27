5625f1699b9b394beff2cdb22baaa1158e329d31
Mraky09 <anhtnt09@gmail.com>
2017-02-27 08:38:23 +0700

    Deploy new themes

--- Gemfile
+++ Gemfile
@@ -6,7 +6,6 @@ git_source(:github) do |repo_name|
 end
 
 gem "rails", "~> 5.0.1"
-gem "sqlite3"
 gem "geocoder"
 gem "puma", "~> 3.0"
 gem "sass-rails", "~> 5.0"
@@ -23,16 +22,24 @@ gem "cancancan"
 gem "bootstrap-sass"
 gem "font-awesome-rails"
 gem "ffaker"
+gem "awesome_print"
+gem "retinajs-rails", "~> 2.1.1"
 
 group :development, :test do
   gem "byebug", platform: :mri
 end
 
 group :development do
+  gem "meta_request"
   gem "web-console", ">= 3.3.0"
   gem "listen", "~> 3.0.5"
   gem "spring"
   gem "spring-watcher-listen", "~> 2.0.0"
+  gem "sqlite3"
 end
 
-gem "tzinfo-data", platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+group :production do
+  gem "pg"
+end
+
+gem "tzinfo-data"
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,9 +11,28 @@
 // about supported directives.
 //
 //= require jquery
+//= require jquery-migrate-1.2.1.min
 //= require bootstrap-sprockets
+//= require retina
+//= require smoothscroll
+//= require markerwithlabel_packed
+//= require infobox
+//= require _owl.carousel.min
+//= require bootstrap-select.min
+//= require jquery.validate.min
+//= require jquery.placeholder
+//= require icheck.min
+//= require jquery.vanillabox-0.1.5.min
+//= require jshashtable-2.1_src
+//= require jquery.numberformatter-1.2.3
+//= require tmpl
+//= require jquery.dependClass-0.1
+//= require draggable-0.1
+//= require jquery.slider
+//= require markerclusterer_packed
+//= require custom-map.js.erb
+//= require custom
 //= require jquery_ujs
 //= require turbolinks
 //= require underscore
-//= require gmaps/google
 //= require_tree .
